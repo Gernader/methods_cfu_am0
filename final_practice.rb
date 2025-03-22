494ce2813d86f73d58a1760fc59e2d5607a8e2e3
William Ross <bill_87_2@yahoo.com>
2025-03-22 15:10:53 -0500

    Complete final practice.rb

--- final_practice.rb
+++ final_practice.rb
@@ -21,8 +21,21 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method? string - "Good Morning"
+# How many arguments did you pass your method? one
+
+def greeting
+  "Good Morning"
+end
+
+first_greeting = greeting
+second_greeting = greeting
+
+puts first_greeting
+puts second_greeting
+
+
+
 
 
 # 2: Write a method named "custom_greeting" that returns a greeting WITH a specific name.
@@ -31,10 +44,19 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? Compound string
+# How many arguments did you pass your method? one plus a paramater
+# What data type was your argument(s)? strings
 
+def custom_greeting(name)
+  "Morning #{name}!"
+end
+
+custom_first = custom_greeting("sam")
+custom_second = custom_greeting("rose")
+
+puts custom_first
+puts custom_second
 
 # 3: Write a method named "greet_person" that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
@@ -42,9 +64,21 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? compound string
+# How many arguments did you pass your method? one plus 3 paramaters
+# What data type was your argument(s)? strings
+
+def greet_person(first, middle, last)
+  "Hi there, #{first} #{middle} #{last}"
+end
+
+person_one =greet_person("Tom", "mate", "oh")
+person_two =greet_person("Sarah", "jean", "connor")
+
+puts person_one
+puts person_two
+
+
 
 
 # 4: Write a method named "square" that takes in one integer, and returns the square of that integer.
@@ -53,15 +87,41 @@ puts last_laugh
 
 # Use the puts or print command to see the return value in the console:
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? int
+# How many arguments did you pass your method? one
+# What data type was your argument(s)? int
+
+
+def square(int)
+  int*int
+end
+
+int_one = 4
+
+square_one =square(int_one)
+square_two =square(6)
+
+puts square_one
+puts square_two
+
+puts "the square of #{int_one} is #{square_one}"
+
 
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
 
 # 5: Write a method named "check_stock" that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(int, ingredint)
+  if int >=4 
+    puts "#{ingredint} is stocked"
+  elsif int ==0
+    puts "#{ingredint} is OUT of stock"
+  else
+    puts "#{ingredint} is running low"
+  end
+end
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
@@ -74,3 +134,4 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
